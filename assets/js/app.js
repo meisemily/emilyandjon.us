4bdf371f33c6acd4a4d7089a0f9b810f8fbfb681
Emily Getman <emily@emilygetman.com>
2013-11-03 10:58:49 -0500

    Start to debug nav selection js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,24 +2,32 @@ $(document).ready(function(){
     $('.scroll').click(function(event){
         event.preventDefault();
         $('html,body').animate({
-            scrollTop:$(this.hash).offset().top}, 500
-        );
+            scrollTop: $(this.hash).offset().top - $('#site_nav').height(),
+        }, 500, 'swing', function(){
+        });
     });
 
-    $('.parallax').parallax("50%", 0.3);
-    $('.ring').parallax("50%", 0.2);
+    $('.parallax').parallax("50%", 0.4);
+    $('.ring').parallax("50%", 0.1);
 });
 
 $(window).scroll(function () {
-    var windowScroll = $(window).scrollTop();
+    var started = $(window).scrollTop();
     var fromBottom = $(document).height() - ($(window).scrollTop() + $(window).height());
+    var boundary = started  + $('#site_nav').height() + $('section').first().height();
 
-    if (fromBottom === 0) {
+    // For testing
+    $('#middle').css({top: boundary});
+
+    if (started === 0) {
+        $('nav a.active').removeClass('active');
+        $('nav a:first').addClass('active');
+    } else if (fromBottom === 0) {
         $('nav a.active').removeClass('active');
         $('nav a:last').addClass('active');
     } else {
         $('section').each(function(i) {
-            if ($(this).position().top <= windowScroll + 150) {
+            if(boundary >= $(this).position().top) {
                 $('nav a.active').removeClass('active');
                 $('nav a').eq(i).addClass('active');
             }
