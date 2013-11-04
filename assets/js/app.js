606045cb5b372866c191d59b15971e19604a5056
Jon Balbarin <jonbalbarin@gmail.com>
2013-11-03 20:24:51 -0500

    wip fix click nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,16 @@
+var Ws = {
+};
+
 $(document).ready(function(){
     $('.scroll').click(function(event){
         event.preventDefault();
+        Ws.lastHash = this.hash;
         $('html,body').animate({
             scrollTop: $(this.hash).offset().top - $('#site_nav').height(),
         }, 500, 'swing', function(){
+          Ws = {
+            lastHash: ""
+          };
         });
     });
 
@@ -12,6 +19,22 @@ $(document).ready(function(){
 });
 
 $(window).scroll(function () {
+    var shouldBail = false;
+    // if doing this from a click, just select the clicked thing
+    if(Ws.lastHash.length) {
+        $('section').each(function(i) {
+            var clickedID = ("#"+$(this).attr('id'));
+            if(clickedID === (Ws.lastHash)) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+                shouldBail = true;
+                return false;
+            }
+        });
+    }
+    if(shouldBail) {
+      return;
+    }
     var started = $(window).scrollTop();
     var fromBottom = $(document).height() - ($(window).scrollTop() + $(window).height());
     var boundary = started  + $('#site_nav').height() + $('section').first().height();
@@ -23,8 +46,8 @@ $(window).scroll(function () {
         $('nav a.active').removeClass('active');
         $('nav a:first').addClass('active');
     } else if (fromBottom === 0) {
-        $('nav a.active').removeClass('active');
-        $('nav a:last').addClass('active');
+          $('nav a.active').removeClass('active');
+          $('nav a:last').addClass('active');
     } else {
         $('section').each(function(i) {
             if(boundary >= $(this).position().top) {
