5ac04ff4fe52614ee823fd7a49625bf1bac097f4
Emily Getman <emily@emilygetman.com>
2014-02-11 22:41:40 -0500

    Rearrange order of loading operations

--- assets/js/app.js
+++ assets/js/app.js
@@ -92,71 +92,10 @@ var Ws = {
         });
       }
     },
-    initializeMaps: function() {
-
-      var setMarkers = function(map, locations) {
-        var infowindow = new google.maps.InfoWindow();
-
-        var markerClick =(function(marker) {
-          return function() {
-            infowindow.setContent(marker.title);
-            infowindow.open(map, marker);
-          };
-        });
-
-        for (var i = 0; i < locations.length; i++) {
-
-          var location = locations[i];
-          var myLatLng = new google.maps.LatLng(location[1], location[2]);
-          var marker = new google.maps.Marker({
-              position: myLatLng,
-              map: map,
-              icon: location[3],
-              title: location[0]
-          });
-
-          google.maps.event.addListener(marker, 'click', markerClick(marker));
-        }
-      };
-      var homeLatlng = new google.maps.LatLng(40.70562,-73.972558);
-
-      var mapOptions = {
-        zoom: 13,
-        scrollwheel: false,
-        mapTypeControl: false,
-        center: homeLatlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-
-      var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-
-      // @todo: make retina
-      var goldStar = {
-        path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-        fillColor: 'yellow',
-        fillOpacity: 1,
-        scale: 0.1,
-        strokeColor: '#c73c30',
-        strokeWeight: 2
-      };
-
-      var places = [
-        ['Brooklyn Winery', 40.717371,-73.955123, goldStar],
-        ['Brooklyn Bridge Marriott', 40.694306,-73.988206]
-        //['Wythe Hotel', 40.722429,-73.95799],
-        //['King & Grove Hotel', 40.721274,-73.955437]
-      ];
-      setMarkers(map, places);
-
-      google.maps.event.addDomListener(window, 'resize', function() {
-          map.setCenter(homeLatlng);
-      });
-    }
 };
 
 $(document).ready(function(){
   Ws.detectIE();
-  Ws.initializeMaps();
   $(window).scroll(Ws.handleScroll).scroll();
   $(window).resize(Ws.handleScroll);
 
